c3a4b643737aecb5a7ec8e661f89d912c4b7ac46
amdla <maciej.m.karabin@gmail.com>
2024-10-21 17:17:42 +0200

    gg

--- lab2/zad2/decrypt_security_ECB_no_entropy.py
+++ lab2/zad2/decrypt_security_ECB_no_entropy.py
@@ -12,10 +12,9 @@ def decrypt_data(encrypted_data, key):
 def decrypt_file(file_name):
     """Decrypt an image file using the key that results in a valid BMP header."""
     key_characters = "abcdefghijklmnopqrstuvwxyz0123456789"
-    valid_decryptions = []
+
     with open(file_name, 'rb') as file:
         encrypted_data = file.read()
-    # Check if data length is a multiple of 16, pad if necessary
     if len(encrypted_data) % 16 != 0:
         needed_padding = 16 - len(encrypted_data) % 16
         encrypted_data += b"\x00" * needed_padding
@@ -24,19 +23,13 @@ def decrypt_file(file_name):
         key = (char * 16).encode('UTF-8')
         decrypted_data = decrypt_data(encrypted_data, key)
         if decrypted_data.startswith(b'BM'):
-            valid_decryptions.append((key.decode(), decrypted_data))
-
-    # Write results to a file
-    with open("valid_decryption_keys.txt", "w") as file:
-        for key, _ in valid_decryptions:
-            file.write(f"Key: {key}\n")
-
-    if valid_decryptions:
-        best_key, best_decrypted_data = valid_decryptions[0]
-        with open(f"decrypted_with_best_key.bmp", "wb") as out_file:
-            out_file.write(best_decrypted_data)
-        print(f"Decrypted using best key '{best_key}'. Output saved as 'decrypted_with_best_key.bmp'.")
+            print(f"Valid BMP header found with key: {key.decode()}. Proceeding with decryption.")
+            with open(f"decrypted_with_best_key.bmp", "wb") as out_file:
+                out_file.write(decrypted_data)
+            print(f"Decrypted using key '{key.decode()}'. Output saved as 'decrypted_with_best_key.bmp'.")
+            break
     else:
+        # If no valid key was found after looping through all possibilities
         print("No valid BMP header found with any of the keys.")
 
 
