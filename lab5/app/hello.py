864e9e67df3042462c36f9caaffd551ae59516b9
Maciej Karabin <maciej.m.karabin@gmail.com>
2024-11-11 22:24:09 +0100

    fixed lab5

--- lab5/app/hello.py
+++ lab5/app/hello.py
@@ -1,35 +1,41 @@
-from flask import Flask, render_template, request, make_response
-import markdown
 from collections import deque
+
+import markdown
+from flask import Flask, render_template, request, make_response
 
 app = Flask(__name__)
 
 notes = []
 recent_users = deque(maxlen=3)
 
+
 @app.route("/")
 def username():
     return render_template("main.html")
+
 
 @app.route("/hello", methods=['GET', 'POST'])
 def hello():
     if request.method == 'POST':
         username = request.form.get("username", "unknown")
-        if not username in recent_users:
+        if username not in recent_users:
             recent_users.append(username)
-        resp = make_response(render_template("hello.html", username=username, notes=notes, recent_users=list(recent_users)))
+        resp = make_response(
+            render_template("hello.html", username=username, notes=notes, recent_users=list(recent_users)))
         resp.set_cookie("username", username)
         return resp
     if request.method == 'GET':
         username = request.cookies.get("username", "unknown")
         return render_template("hello.html", username=username, notes=notes, recent_users=list(recent_users))
 
+
 @app.route("/render", methods=['POST'])
 def render():
-    md = request.form.get("markdown","")
+    md = request.form.get("markdown", "")
     rendered = markdown.markdown(md)
     notes.append(rendered)
     return render_template("markdown.html", rendered=rendered)
+
 
 @app.route("/render/<rendered_id>")
 def render_old(rendered_id):
