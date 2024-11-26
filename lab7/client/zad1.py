9582138df3274a33ff3b307bf202ad68c4206212
amdla <maciej.m.karabin@gmail.com>
2024-11-26 13:07:47 +0100

    finished lab7

--- lab7/client/zad1.py
+++ lab7/client/zad1.py
@@ -1,11 +1,10 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
-import base64
 from client import Client
 
 client = Client('http://localhost:5555')
 
-with open("key.1", "rb") as f:
+with open("../server/key.1", "rb") as f:
     pub_key = RSA.import_key(f.read())
 
 cipher = PKCS1_OAEP.new(pub_key)
--- lab7/client/zad2.py
+++ lab7/client/zad2.py
@@ -1,12 +1,31 @@
+from Crypto.PublicKey import RSA
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
+import base64
 import client
 
-client = client.Client('http://10.42.12.148:5555')
-client.get_key("deadbeef")
-# pub
+server_url = "http://localhost:5555"
+client = client.Client(server_url)
+
+with open("../server/key.1", "rb") as f:
+    pub_key = RSA.import_key(f.read())
+
 with open("../server/key.2", "rb") as f:
-    private_key = f.read()
+    private_key = RSA.import_key(f.read())
 
-client.send_binary_message("deadbeef", private_key)
+message = "brglauigas"
+hashed_message = SHA256.new(message.encode())
 
-# wygeneruj wiadomosc z podpisem, zweryfikuj go
-# mamy swoja wiadomosc zahashowac sha256, a potem zaszyfrowac kluczem prywatnym
+signature = pkcs1_15.new(private_key).sign(hashed_message)
+encoded_signature = base64.b64encode(signature).decode()
+
+client.send_text_message("abc", message + "|" + encoded_signature)
+
+received_message = client.get_text_message("abc")
+original_message, received_signature = received_message.split('|')
+decoded_signature = base64.b64decode(received_signature.encode())
+hashed_received_message = SHA256.new(original_message.encode())
+pkcs1_15.new(pub_key).verify(hashed_received_message, decoded_signature)
+
+print(original_message)
+print(received_signature)
